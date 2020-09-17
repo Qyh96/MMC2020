a1bdf02dc381b208bea9ae7026594abccd783a63
qiuyanhui <qiuyanhui@jd.com>
2020-09-17 12:48:31 +0800

    创建maven项目

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @author qiuyanhui
+ * @version 1.0
+ * @date 2020/9/17 12:46
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
+
